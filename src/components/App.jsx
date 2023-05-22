bd15b5f9b3a04ba9aa4bb7f07cd2849f779f8563
Ndanganeni Nedzamba <94652882+Ndanga02@users.noreply.github.com>
2023-05-22 05:26:15 +0200

    shortened code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,43 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [todo, setTodo] = useState([]);
 
-const [input,setInput]=useState("")  
-const [todo,setTodo]=useState([])
+  function handleChange(event) {
+    setInput(event.target.value);
+  }
 
+  function addNotes() {
+    setTodo([...todo, input]);
+  }
 
-  function handleChange(event){
-setInput(event.target.value)
-}
+  function TodoList() {
+    return (
+      <ul>
+        {todo.map((task) => (
+          <li>{task}</li>
+        ))}
+      </ul>
+    );
+  }
 
-function addNotes(){
-  setTodo([...todo,input])
-}
-
-function TodoList() {
   return (
-    <ul>
-      {todo.map((task) => (
-        <li>{task}</li>
-      ))}
-    </ul>
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input name="input" onChange={handleChange} type="text" />
+        <button name="pushBtn" onClick={addNotes}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <TodoList />
+      </div>
+    </div>
   );
-}
-
-
-return (
-  <div className="container">
-    <div className="heading">
-      <h1>To-Do List</h1>
-    </div>
-    <div className="form">
-      <input name="input" onChange={handleChange} type="text" />
-      <button name="pushBtn" onClick={addNotes}>
-        <span>Add</span>
-      </button >
-    </div>
-    <div>
-     <TodoList/> 
-    </div>
-  </div>
-);
 }
 
 export default App;
