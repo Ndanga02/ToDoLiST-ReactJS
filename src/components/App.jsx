37fc93c25f62896ae66f48a76ae3a2456deda8f2
Ndanga <ndanganedz@gmail.com>
2023-05-22 05:15:11 +0200

    add notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,46 @@
-import React from "react";
+import React,{useState} from "react";
 
 function App() {
+
+const [input,setInput]=useState("")  
+const [todo,setTodo]=useState([])
+
+
+  function handleChange(event){
+setInput(event.target.value)
+}
+
+function addNotes(){
+  setTodo([...todo,input])
+}
+
+function TodoList() {
   return (
-    <div className="container">
-      <div className="heading">
-        <h1>To-Do List</h1>
-      </div>
-      <div className="form">
-        <input type="text" />
-        <button>
-          <span>Add</span>
-        </button>
-      </div>
-      <div>
-        <ul>
-          <li>A Item</li>
-        </ul>
-      </div>
-    </div>
+    <ul>
+      {todo.map((task) => (
+        <li>{task}</li>
+      ))}
+    </ul>
   );
+}
+
+
+return (
+  <div className="container">
+    <div className="heading">
+      <h1>To-Do List</h1>
+    </div>
+    <div className="form">
+      <input name="input" onChange={handleChange} type="text" />
+      <button name="pushBtn" onClick={addNotes}>
+        <span>Add</span>
+      </button >
+    </div>
+    <div>
+     <TodoList/> 
+    </div>
+  </div>
+);
 }
 
 export default App;
